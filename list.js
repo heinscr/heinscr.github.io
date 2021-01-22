9dc7875dd38dbbcbf5910c6c0f92039f9fce843c
Craig Heins <cheins@meditech.com>
2021-01-22 09:56:14 -0500

    Updated Readme file

--- list.js
+++ list.js
@@ -271,7 +271,7 @@ function prepareTable(info) {
   var cols = [45, 30, 15];
   var content = [];
   content.push(padRight('Last Modified', cols[1]) + '  ' +
-               padRight('Size', cols[2]) + 'Key \n');
+               padRight('Size', cols[2]) + 'Book Name \n');
   content.push(new Array(cols[0] + cols[1] + cols[2] + 4).join('-') + '\n');
 
   // add ../ at the start of the dir listing, unless we are already at root dir

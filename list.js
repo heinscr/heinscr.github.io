9c74dca78848967d8e632c6fb0281ef8ab5e2816
Craig Heins <cheins@meditech.com>
2021-01-25 19:44:14 -0500

    Book for column name

--- list.js
+++ list.js
@@ -1,185 +1,188 @@
 if (typeof AUTO_TITLE != 'undefined' && AUTO_TITLE == true) {
   document.title = location.hostname;
-}
-
-if (typeof S3_REGION != 'undefined') {
+  }
+  
+  if (typeof S3_REGION != 'undefined') {
   var BUCKET_URL = location.protocol + '//' + location.hostname + '.' + S3_REGION + '.amazonaws.com'; // e.g. just 's3' for us-east-1 region
   var BUCKET_WEBSITE_URL = location.protocol + '//' + location.hostname;
-}
-
-if (typeof S3BL_IGNORE_PATH == 'undefined' || S3BL_IGNORE_PATH != true) {
+  }
+  
+  if (typeof S3BL_IGNORE_PATH == 'undefined' || S3BL_IGNORE_PATH != true) {
   var S3BL_IGNORE_PATH = false;
-}
-
-if (typeof BUCKET_URL == 'undefined') {
+  }
+  
+  if (typeof BUCKET_URL == 'undefined') {
   var BUCKET_URL = location.protocol + '//' + location.hostname;
-}
-
-if (typeof BUCKET_NAME != 'undefined') {
+  }
+  
+  if (typeof BUCKET_NAME != 'undefined') {
   // if bucket_url does not start with bucket_name,
   // assume path-style url
   if (!~BUCKET_URL.indexOf(location.protocol + '//' + BUCKET_NAME)) {
     BUCKET_URL += '/' + BUCKET_NAME;
   }
-}
-
-if (typeof BUCKET_WEBSITE_URL == 'undefined') {
+  }
+  
+  if (typeof BUCKET_WEBSITE_URL == 'undefined') {
   var BUCKET_WEBSITE_URL = BUCKET_URL;
-}
-
-if (typeof S3B_ROOT_DIR == 'undefined') {
+  }
+  
+  if (typeof S3B_ROOT_DIR == 'undefined') {
   var S3B_ROOT_DIR = '';
-}
-
-if (typeof S3B_SORT == 'undefined') {
+  }
+  
+  if (typeof S3B_SORT == 'undefined') {
   var S3B_SORT = 'DEFAULT';
-}
-
-if (typeof EXCLUDE_FILE == 'undefined') {
+  }
+  
+  if (typeof EXCLUDE_FILE == 'undefined') {
   var EXCLUDE_FILE = [];
-} else if (typeof EXCLUDE_FILE == 'string' || EXCLUDE_FILE instanceof RegExp) {
+  } else if (typeof EXCLUDE_FILE == 'string' || EXCLUDE_FILE instanceof RegExp) {
   var EXCLUDE_FILE = [EXCLUDE_FILE];
-}
-
-// https://tc39.github.io/ecma262/#sec-array.prototype.includes
-if (!Array.prototype.includes) {
+  }
+  
+  // https://tc39.github.io/ecma262/#sec-array.prototype.includes
+  if (!Array.prototype.includes) {
   Object.defineProperty(Array.prototype, 'includes', {
     value: function(searchElement, fromIndex) {
-
-      if (this == null) {
-        throw new TypeError('"this" is null or not defined');
-      }
-
-      // 1. Let O be ? ToObject(this value).
-      var o = Object(this);
-
-      // 2. Let len be ? ToLength(? Get(O, "length")).
-      var len = o.length >>> 0;
-
-      // 3. If len is 0, return false.
-      if (len === 0) {
-        return false;
-      }
-
-      // 4. Let n be ? ToInteger(fromIndex).
-      //    (If fromIndex is undefined, this step produces the value 0.)
-      var n = fromIndex | 0;
-
-      // 5. If n ≥ 0, then
-      //  a. Let k be n.
-      // 6. Else n < 0,
-      //  a. Let k be len + n.
-      //  b. If k < 0, let k be 0.
-      var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
-
-      function sameValueZero(x, y) {
-        return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
-      }
-
-      // 7. Repeat, while k < len
-      while (k < len) {
-        // a. Let elementK be the result of ? Get(O, ! ToString(k)).
-        // b. If SameValueZero(searchElement, elementK) is true, return true.
-        if (sameValueZero(o[k], searchElement)) {
-          return true;
-        }
-        // c. Increase k by 1. 
-        k++;
-      }
-
-      // 8. Return false
+  
+    if (this == null) {
+      throw new TypeError('"this" is null or not defined');
+    }
+  
+    // 1. Let O be ? ToObject(this value).
+    var o = Object(this);
+  
+    // 2. Let len be ? ToLength(? Get(O, "length")).
+    var len = o.length >>> 0;
+  
+    // 3. If len is 0, return false.
+    if (len === 0) {
       return false;
     }
+  
+    // 4. Let n be ? ToInteger(fromIndex).
+    //    (If fromIndex is undefined, this step produces the value 0.)
+    var n = fromIndex | 0;
+  
+    // 5. If n ≥ 0, then
+    //  a. Let k be n.
+    // 6. Else n < 0,
+    //  a. Let k be len + n.
+    //  b. If k < 0, let k be 0.
+    var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+  
+    function sameValueZero(x, y) {
+      return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+    }
+  
+    // 7. Repeat, while k < len
+    while (k < len) {
+      // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+      // b. If SameValueZero(searchElement, elementK) is true, return true.
+      if (sameValueZero(o[k], searchElement)) {
+      return true;
+      }
+      // c. Increase k by 1. 
+      k++;
+    }
+  
+    // 8. Return false
+    return false;
+    }
   });
-}
-
-jQuery(function($) { getS3Data(); });
-
-// This will sort your file listing by most recently modified.
-// Flip the comparator to '>' if you want oldest files first.
-function sortFunction(a, b) {
+  }
+  
+  jQuery(function($) { getS3Data(); });
+  
+  // This will sort your file listing by most recently modified.
+  // Flip the comparator to '>' if you want oldest files first.
+  function sortFunction(a, b) {
   switch (S3B_SORT) {
     case "OLD2NEW":
-      return a.LastModified > b.LastModified ? 1 : -1;
+    return a.LastModified > b.LastModified ? 1 : -1;
     case "NEW2OLD":
-      return a.LastModified < b.LastModified ? 1 : -1;
+    return a.LastModified < b.LastModified ? 1 : -1;
     case "A2Z":
-      return a.Key < b.Key ? 1 : -1;
+    return a.Key < b.Key ? 1 : -1;
     case "Z2A":
-      return a.Key > b.Key ? 1 : -1;
+    return a.Key > b.Key ? 1 : -1;
     case "BIG2SMALL":
-      return a.Size < b.Size ? 1 : -1;
+    return a.Size < b.Size ? 1 : -1;
     case "SMALL2BIG":
-      return a.Size > b.Size ? 1 : -1;
+    return a.Size > b.Size ? 1 : -1;
   }
-}
-
-function getS3Data(marker, html) {
+  }
+  
+  function getS3Data(marker, html) {
   var s3_rest_url = createS3QueryUrl(marker);
   // set loading notice
   $('#listing')
-      .html('<img src="//assets.okfn.org/images/icons/ajaxload-circle.gif" />');
+    .html('<img src="//assets.okfn.org/images/icons/ajaxload-circle.gif" />');
   $.get(s3_rest_url)
-      .done(function(data) {
-        // clear loading notice
-        $('#listing').html('');
-        var xml = $(data);
-        var info = getInfoFromS3Data(xml);
+    .done(function(data) {
+      // clear loading notice
+      $('#listing').html('');
+      var xml = $(data);
+      var info = getInfoFromS3Data(xml);
+  
+      // Slight modification by FuzzBall03
+      // This will sort your file listing based on var S3B_SORT
+      // See url for example:
+      // http://esp-link.s3-website-us-east-1.amazonaws.com/
+      if (S3B_SORT != 'DEFAULT') {
+      var sortedFiles = info.files;
+      sortedFiles.sort(sortFunction);
+      info.files = sortedFiles;
+      }
+  
+      buildNavigation(info);
+  
+      // Add a <base> element to the document head to make relative links
+      // work even if the URI does not contain a trailing slash
+      var base = window.location.href
+      base = (base.endsWith('/')) ? base : base + '/';
+      $('head').append('<base href="' + base + '">');
+  
+      html = typeof html !== 'undefined' ? html + prepareTable(info) :
+                         prepareTable(info);
+      if (info.nextMarker != "null") {
+      getS3Data(info.nextMarker, html);
+      } else {
+      document.getElementById('listing').innerHTML =
+        '<table id="myTable" class="sortable">' + html + '</table>';
+      }
 
-        // Slight modification by FuzzBall03
-        // This will sort your file listing based on var S3B_SORT
-        // See url for example:
-        // http://esp-link.s3-website-us-east-1.amazonaws.com/
-        if (S3B_SORT != 'DEFAULT') {
-          var sortedFiles = info.files;
-          sortedFiles.sort(sortFunction);
-          info.files = sortedFiles;
-        }
-
-        buildNavigation(info);
-
-        // Add a <base> element to the document head to make relative links
-        // work even if the URI does not contain a trailing slash
-        var base = window.location.href
-        base = (base.endsWith('/')) ? base : base + '/';
-        $('head').append('<base href="' + base + '">');
-
-        html = typeof html !== 'undefined' ? html + prepareTable(info) :
-                                             prepareTable(info);
-        if (info.nextMarker != "null") {
-          getS3Data(info.nextMarker, html);
-        } else {
-          document.getElementById('listing').innerHTML =
-              '<pre>' + html + '</pre>';
-        }
-      })
-      .fail(function(error) {
-        console.error(error);
-        $('#listing').html('<strong>Error: ' + error + '</strong>');
-      });
-}
-
-function buildNavigation(info) {
+    var newTableObject = document.getElementById('myTable');
+    sorttable.makeSortable(newTableObject);
+  })
+    .fail(function(error) {
+      console.error(error);
+      $('#listing').html('<strong>Error: ' + error + '</strong>');
+    });
+  }
+  
+  function buildNavigation(info) {
   var baseUrl = S3BL_IGNORE_PATH == false ? '/' : '?prefix=';
   var root = '<a href="' + baseUrl + '">' + BUCKET_WEBSITE_URL + '</a> / ';
   if (info.prefix) {
     var processedPathSegments = '';
     var content = $.map(info.prefix.split('/'), function(pathSegment) {
-      processedPathSegments =
-          processedPathSegments + encodeURIComponent(pathSegment) + '/';
-      return '<a href="' + baseUrl + processedPathSegments.replace(/"/g, '&quot;') + '">' +
-             pathSegment + '</a>';
+    processedPathSegments =
+      processedPathSegments + encodeURIComponent(pathSegment) + '/';
+    return '<a href="' + baseUrl + processedPathSegments.replace(/"/g, '&quot;') + '">' +
+         pathSegment + '</a>';
     });
     $('#navigation').html(root + content.join(' / '));
   } else {
     $('#navigation').html(root);
   }
-}
-
-function createS3QueryUrl(marker) {
+  }
+  
+  function createS3QueryUrl(marker) {
   var s3_rest_url = BUCKET_URL;
   s3_rest_url += '?delimiter=/';
-
+  
   //
   // Handling paths and prefixes:
   //
@@ -193,7 +196,7 @@ function createS3QueryUrl(marker) {
   // Why both? Because we want classic directory style listing in normal
   // buckets but also allow deploying to non-buckets
   //
-
+  
   var rx = '.*[?&]prefix=' + S3B_ROOT_DIR + '([^&]+)(&.*)?$';
   var prefix = '';
   if (S3BL_IGNORE_PATH == false) {
@@ -204,7 +207,7 @@ function createS3QueryUrl(marker) {
     prefix = S3B_ROOT_DIR + match[1];
   } else {
     if (S3BL_IGNORE_PATH) {
-      var prefix = S3B_ROOT_DIR;
+    var prefix = S3B_ROOT_DIR;
     }
   }
   if (prefix) {
@@ -216,18 +219,18 @@ function createS3QueryUrl(marker) {
     s3_rest_url += '&marker=' + marker;
   }
   return s3_rest_url;
-}
-
-function getInfoFromS3Data(xml) {
+  }
+  
+  function getInfoFromS3Data(xml) {
   var prefix = $(xml.find('Prefix')[0]).text();
   var files = $.map(xml.find('Contents'), function(item) {
     item = $(item);
     // clang-format off
     return {
-      Key: item.find('Key').text(),
-          LastModified: item.find('LastModified').text(),
-          Size: bytesToHumanReadable(item.find('Size').text()),
-          Type: 'file'
+    Key: item.find('Key').text(),
+      LastModified: item.find('LastModified').text(),
+      Size: bytesToHumanReadable(item.find('Size').text()),
+      Type: 'file'
     }
     // clang-format on
   });
@@ -238,10 +241,10 @@ function getInfoFromS3Data(xml) {
     item = $(item);
     // clang-format off
     return {
-      Key: item.find('Prefix').text(),
-        LastModified: '',
-        Size: '0',
-        Type: 'directory'
+    Key: item.find('Prefix').text(),
+      LastModified: '',
+      Size: '0',
+      Type: 'directory'
     }
     // clang-format on
   });
@@ -258,74 +261,77 @@ function getInfoFromS3Data(xml) {
     nextMarker: encodeURIComponent(nextMarker)
   }
   // clang-format on
-}
-
-// info is object like:
-// {
-//    files: ..
-//    directories: ..
-//    prefix: ...
-// }
-function prepareTable(info) {
+  }
+  
+  // info is object like:
+  // {
+  //    files: ..
+  //    directories: ..
+  //    prefix: ...
+  // }
+  function prepareTable(info) {
   var files = info.directories.concat(info.files), prefix = info.prefix;
   var cols = [45, 30, 15];
   var content = [];
-  content.push(padRight('Books',cols[0])+
-	  		   padRight('Last Modified', cols[1]) + '  ' +
-			   'Size\n');
-  content.push(new Array(cols[0] + cols[1] + cols[2] + 4).join('-') + '\n');
-
+  content.push('<tr>'+
+         '<td>Books</td>'+
+         '<td>Last Modified</td>'+
+         '<td>Size</td>'+
+         '</tr>');
+  //content.push(new Array(cols[0] + cols[1] + cols[2] + 4).join('-') + '\n');
+  
   // add ../ at the start of the dir listing, unless we are already at root dir
   if (prefix && prefix !== S3B_ROOT_DIR) {
     var up = prefix.replace(/\/$/, '').replace(/"/g, '&quot;').split('/').slice(0, -1).concat('').join(
-            '/'),  // one directory up
-        item =
-            {
-              Key: up,
-              LastModified: '',
-			  Size: '',
-			  keyText,
-              href: S3BL_IGNORE_PATH ? '?prefix=' + up : '../'
-            },
-        row = renderRow(item, cols);
-    content.push(row + '\n');
+        '/'),  // one directory up
+      item =
+        {
+        Key: up,
+        LastModified: '',
+        Size: '',
+        keyText,
+        href: S3BL_IGNORE_PATH ? '?prefix=' + up : '../'
+        },
+      row = renderRow(item, cols);
+   content.push(row + '\n');
   }
-
+  
   jQuery.each(files, function(idx, item) {
     // strip off the prefix
     item.keyText = item.Key.substring(prefix.length);
     if (item.Type === 'directory') {
-      if (S3BL_IGNORE_PATH) {
-        item.href = location.protocol + '//' + location.hostname +
-                    location.pathname + '?prefix=' + encodePath(item.Key);
-      } else {
-        item.href = encodePath(item.keyText);
-      }
+    if (S3BL_IGNORE_PATH) {
+      item.href = location.protocol + '//' + location.hostname +
+            location.pathname + '?prefix=' + encodePath(item.Key);
     } else {
-      item.href = BUCKET_WEBSITE_URL + '/' + encodePath(item.Key);
+      item.href = encodePath(item.keyText);
+    }
+    } else {
+    item.href = BUCKET_WEBSITE_URL + '/' + encodePath(item.Key);
     }
     var row = renderRow(item, cols);
     if (!EXCLUDE_FILE.some(function(exclude){ return testExcludeFilter(exclude, item.Key); }))
-      content.push(row + '\n');
+    content.push(row + '\n');
   });
-
+  
   return content.join('');
-}
-
-// Encode everything but "/" which are significant in paths and to S3
-function encodePath(path) {
+  }
+  
+  // Encode everything but "/" which are significant in paths and to S3
+  function encodePath(path) {
   return encodeURIComponent(path).replace(/%2F/g, '/')
-}
-
-function renderRow(item, cols) {
-  var row = '';
-  row += '<a href="' + item.href + '">' + padRight(item.keyText,cols[0]) + '</a>';
-  row += padRight(item.LastModified, cols[1]) + '  ';
-  row += padRight(item.Size, cols[2]);
+  }
+  
+  function renderRow(item, cols) {
+  var row = '<tr>';
+  row += '<td><a href="' + item.href + '">' + item.keyText + '</a></td>';
+  row += '<td>'+item.LastModified+'</td>';
+  row += '<td>'+item.Size+'</td>';
+  row+= '</tr>';
   return row;
-}
-
-function padRight(padString, length) {
+  }
+  
+  function padRight(padString, length) {
   var str = padString.slice(0, length - 3);
   if (padString.length > str.length) {
     str += '...';
@@ -334,9 +340,9 @@ function padRight(padString, length) {
     str = str + ' ';
   }
   return str;
-}
-
-function bytesToHumanReadable(sizeInBytes) {
+  }
+  
+  function bytesToHumanReadable(sizeInBytes) {
   var i = -1;
   var units = [' kB', ' MB', ' GB'];
   do {
@@ -344,9 +350,9 @@ function bytesToHumanReadable(sizeInBytes) {
     i++;
   } while (sizeInBytes > 1024);
   return Math.max(sizeInBytes, 0.1).toFixed(1) + units[i];
-}
-
-function testExcludeFilter(filter, key) {
+  }
+  
+  function testExcludeFilter(filter, key) {
   if (typeof filter == 'string') {
     return key == filter;
   }
@@ -357,4 +363,4 @@ function testExcludeFilter(filter, key) {
   {
     throw "exclude filter is not a string or regexp";
   }
-}
+  }
